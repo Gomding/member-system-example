005c9a289b5871cf65a6ab1a6408970495a153cb
Gomding <backchida@gmail.com>
2023-03-28 22:21:06 +0900

    feature: Member 엔티티에 equals hashcode 재정의

--- src/main/kotlin/com/member/persistence/Member.kt
+++ src/main/kotlin/com/member/persistence/Member.kt
@@ -23,4 +23,19 @@ class Member(
     @Column
     var age: Int = age
     protected set
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Member
+
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
 }
